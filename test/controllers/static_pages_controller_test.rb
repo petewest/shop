f9ef0d5a7f34b26af4c2ee6c0659f1bb8e47686c
Peter West <source@petewest.org>
2014-03-24 20:56:36 +0000

    home page

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "h1", "Welcome to the shop"
+  end
+
+end
