fd37f2c6b3342b4736df795addb69d221704b392
Peter West <source@petewest.org>
2014-03-30 09:28:46 +0100

    Refactored action bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,12 +3,16 @@ module ApplicationHelper
     "Shop!" + (t.blank? ? "" : " | #{t}")
   end
 
-  def seller_actions(&block)
-    html=%Q{<div class="seller_actions">
-      <h4>Seller actions</h4>
+  def action_bar(item, options={}, &block)
+    options[:class]||="action_bar"
+    options[:title]||="Actions"
+    html=%Q{<div class="#{options[:class]}">
+      #{content_tag(:h4, options[:title])}
       <div class="btn-group-vertical">
     }
-    html<<capture(&block)
+    html<<capture(&block) if block_given?
+    html<<link_to("Edit", [:edit, item], class: "btn btn-default")
+    html<<link_to("Delete", item, method: :delete, data: {confirm: "Are you sure you wish to delete this #{item.class.name.downcase}?"}, class: "btn btn-danger")
     html<<%Q{</div></div>}
     html.html_safe
   end
