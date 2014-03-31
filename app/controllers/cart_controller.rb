6bc051d1f5209d856b987c5d2018bb0cb07afeea
Peter West <source@petewest.org>
2014-03-31 21:45:46 +0100

    Remove items from cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,10 +1,14 @@
 class CartController < ApplicationController
   def index
-    @products=Product.find(current_cart.map{|k,v| v[:product_id]})
+    @products=Product.find(current_cart.map{|k,v| v["product_id"]})
   end
 
   def destroy
     remove_from_cart(params[:id])
-    redirect_to cart_path
+    flash[:success]="Item removed from cart"
+    respond_to do |format|
+      format.html { redirect_to cart_path }
+      format.js
+    end
   end
 end
