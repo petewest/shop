35f83b8d7ffadf993912fe8fa4a43b53173df102
Peter West <source@petewest.org>
2014-03-31 22:13:15 +0100

    Update action

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -5,10 +5,22 @@ class CartController < ApplicationController
 
   def destroy
     remove_from_cart(params[:id])
-    flash[:success]="Item removed from cart"
+    flash.now[:success]="Item removed from cart"
     respond_to do |format|
       format.html { redirect_to cart_path }
       format.js
     end
   end
+
+  def update
+    self.current_cart=current_cart.merge(params[:id] => current_cart[params[:id]].merge(cart_params)) if current_cart.has_key?(params[:id])
+    respond_to do |format|
+      format.html {redirect_to cart_path}
+    end
+  end
+
+  private
+    def cart_params
+      params.permit(:quantity)
+    end
 end
--- test/controllers/cart_controller_test.rb
+++ test/controllers/cart_controller_test.rb
@@ -11,6 +11,30 @@ class CartControllerTest < ActionController::TestCase
     assert_difference "current_cart.count", -1 do
       delete :destroy, id: current_cart.map{|k,v| k}.first
     end
+    assert_equal "Item removed from cart", flash[:success]
+  end
+
+  test "should remove item from cart (js)" do
+    add_to_cart(products(:tshirt), 1)
+    assert_difference "current_cart.count", -1 do
+      delete :destroy, format: :js, id: current_cart.map{|k,v| k}.first
+    end
+  end
+
+  test "should change quantity" do
+    product=products(:tshirt)
+    add_to_cart(product, 1)
+    id=current_cart.map{|k,v| k}.first
+    patch :update, id: id, quantity: "2"
+    assert_equal Hash(product_id: product.id, quantity: "2").stringify_keys, current_cart[id]
+  end
+
+  test "should not change product id" do
+    product=products(:tshirt)
+    add_to_cart(product, 1)
+    id=current_cart.map{|k,v| k}.first
+    patch :update, id: id, product_id: products(:product_1).id
+    assert_equal Hash(product_id: product.id, quantity: 1).stringify_keys, current_cart[id]
   end
 
 end
