8eddc0f0fd6d82684b99c16d9e70c6a0a9858489
Peter West <source@petewest.org>
2014-04-01 14:15:49 +0100

    Adding an item already in the cart increases quantity

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -9,8 +9,12 @@ module CartHelper
   end
 
   def add_to_cart(item, quantity)
+    item_hash={product_id: item.id, quantity: quantity}
+    match_keys=item_hash.except(:quantity).keys
     cart_now=current_cart
-    cart_now[SecureRandom.hex(5)]={product_id: item.id, quantity: quantity}
+    current_key=cart_now.select{|k,v| match_keys.all?{|i| v.symbolize_keys[i]==item_hash[i]}}.keys.first
+    current_key||=SecureRandom.hex(5)
+    cart_now[current_key]=item_hash.merge((cart_now[current_key] || {}).symbolize_keys.except(*match_keys)){|key, old, new| old+new}
     self.current_cart=cart_now
   end
 
