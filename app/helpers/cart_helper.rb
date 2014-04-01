a8eafab61355ad806c8ed2ff1ba65b4465a4fb2d
Peter West <source@petewest.org>
2014-04-01 15:13:17 +0100

    Add commens

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -9,11 +9,17 @@ module CartHelper
   end
 
   def add_to_cart(item, quantity)
+    #construct item hash
     item_hash={product_id: item.id, quantity: quantity}
+    #match keys will be every item in the product hash except quantity
+    #this way it'll only increase the quantity when everything else is the same
     match_keys=item_hash.except(:quantity).keys
     cart_now=current_cart
+    #do we have an existing item?
     current_key=cart_now.select{|k,v| match_keys.all?{|i| v.symbolize_keys[i]==item_hash[i]}}.keys.first
+    #if not, we'll create a new one
     current_key||=SecureRandom.hex(5)
+    #add the line item (or add the quantity to the existing one)
     cart_now[current_key]=item_hash.merge((cart_now[current_key] || {}).symbolize_keys.except(*match_keys)){|key, old, new| old+new}
     self.current_cart=cart_now
   end
