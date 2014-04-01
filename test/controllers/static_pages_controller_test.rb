de8bf88a793e46f99a1100870ed26d3a9338b318
Peter West <source@petewest.org>
2014-04-01 23:13:57 +0100

    Test to make sure seller options aren't present if not seller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,6 +10,16 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "a[href=#{root_url}]", "Shop!"
     assert_select "a[href=#{signin_path}]", "Sign in"
     assert_select "a[href=#{signup_path}]", "Sign up"
+    assert_select "a[href=#{new_product_path}]", 0
+  end
+
+  test "should have different links when signed in as buyer" do
+    sign_in users(:buyer)
+    get :home
+    assert_select "a[href=#{signin_path}]", 0
+    assert_select "a[href=#{signup_path}]", 0
+    assert_select "a[href=#{new_product_path}]", 0
+    assert_select "a[href=#{signout_path}]"
   end
 
   test "should show seller menu when logged in as seller" do
@@ -17,6 +27,7 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :home
     assert_select "a", "Seller"
     assert_select "a[href=#{new_product_path}]"
+    assert_select "a[href=#{signout_path}]"
   end
 
 end
