237be58aaece50beb0ed396ad8fc1811a24d462e
Peter West <source@petewest.org>
2014-04-01 21:38:45 +0100

    Move s3 endpoint to secrets

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 
 
-Paperclip::Attachment.default_options[:s3_host_name]='s3-eu-west-1.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name]=Rails.application.secrets.s3_endpoint if Rails.application.secrets.s3_endpoint
