064ba8afbb2994d4c381ba52f94da19e7fd0dc5a
Peter West <source@petewest.org>
2014-04-01 21:15:55 +0100

    Creating new user signs in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user=User.new(user_params)
     if @user.save
       flash[:success]="Welcome #{@user.name}"
+      sign_in @user
       redirect_to root_path
     else
       flash.now[:danger]="New user creation failed"
