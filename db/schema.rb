87465e52f12e03532d7c83062cd9d7a1c6033ad3
Peter West <source@petewest.org>
2014-04-02 20:01:38 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140402105127) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "costs", force: true do |t|
     t.integer  "costable_id"
     t.string   "costable_type"
