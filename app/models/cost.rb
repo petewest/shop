312c1849024bd3777b1ff8b453cf6520eb1e1306
Peter West <source@petewest.org>
2014-04-02 17:11:48 +0100

    Move costable validation to before_save hook as it makes more sense here

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -2,16 +2,17 @@ class Cost < ActiveRecord::Base
   belongs_to :costable, polymorphic: true, inverse_of: :cost
   belongs_to :currency
 
-  validates :costable_id, presence: true, uniqueness: {scope: :costable_type}, unless: :costable_new?
+  validates :costable_id, uniqueness: {scope: :costable_type}
   validates :currency, presence: true
   validates :value, presence: true, numericality: {only_integer: true}
 
+  before_save :validate_costable
+
   private
-    def costable_new?
-      #stolen from stack overflow to skip this validation if the parent item is new
-      if new_record? and !costable and costable_type
-        costable=ObjectSpace.each_object(costable_type.constantize).find{|o| o.cost==self} 
+    def validate_costable
+      if !costable
+        errors[:costable]<<"can't be blank"
+        false
       end
-      costable
     end
 end
