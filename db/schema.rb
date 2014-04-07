ac821ae59a77baf472838d781174a4edc6a9d835
Peter West <source@petewest.org>
2014-04-07 09:02:58 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140406184203) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "currencies", force: true do |t|
     t.string   "iso_code"
     t.string   "symbol"
