9aad42067612fd64f66ce6b3b310a68f4a9734f0
Peter West <source@petewest.org>
2014-04-07 12:56:13 +0100

    stock level validations

--- app/models/stock_level.rb
+++ app/models/stock_level.rb
@@ -3,6 +3,12 @@ class StockLevel < ActiveRecord::Base
 
   validates :product, presence: true
   validates :due_at, presence: true
+  validates :start_quantity, presence: true, numericality: {only_integer: true}
+  validates :current_quantity, numericality: {only_integer: true, greater_than_or_equal_to: 0}, allow_nil: true
+
+  validate :current_lte_start
+
+  before_save -> { self.current_quantity||=start_quantity }
 
   def self.current
     where(
@@ -13,4 +19,10 @@ class StockLevel < ActiveRecord::Base
       arel_table[:expires_at].gt(DateTime.now).or(arel_table[:expires_at].eq(nil))
     )
   end
+
+  private
+    def current_lte_start
+      errors.add(:current_quantity, "must be less than or equal to start quantity") if current_quantity and current_quantity>start_quantity
+    end
+
 end
--- test/models/stock_level_test.rb
+++ test/models/stock_level_test.rb
@@ -35,6 +35,54 @@ class StockLevelTest < ActiveSupport::TestCase
     assert_respond_to sl, :expires_at
   end
 
+  test "should fail to save if start_quantity is not a number" do
+    sl=StockLevel.new(valid.merge(start_quantity: "elephant"))
+    assert_not sl.save
+  end
+
+  test "should fail to save if current_quantity is not a number" do
+    sl=StockLevel.new(valid.merge(current_quantity: "elephant"))
+    assert_not sl.save
+  end
+
+  test "should fail to save if start_quantity is not an integer" do
+    sl=StockLevel.new(valid.merge(start_quantity: 10.5))
+    assert_not sl.save
+  end
+
+  test "should fail to save if current_quantity is not an integer" do
+    sl=StockLevel.new(valid.merge(current_quantity: 10.5))
+    assert_not sl.save
+  end
+
+  test "should fail to save without start_quantity" do
+    sl=StockLevel.new(valid.except(:start_quantity))
+    assert_not sl.save
+  end
+
+  test "should set current_quantity to be start if current is nil" do
+    sl=StockLevel.new(valid.except(:current_quantity))
+    assert sl.save
+    assert_equal sl.start_quantity, sl.current_quantity
+  end
+
+  test "should not overwrite current quantity if already set" do
+    sl=StockLevel.new(valid.merge(current_quantity: 5))
+    sl.save
+    sl.reload
+    assert_equal 5, sl.current_quantity
+  end
+
+  test "should not allow current to be more than start" do
+    sl=StockLevel.new(valid)
+    sl.current_quantity=sl.start_quantity+1
+    assert_not sl.save
+  end
+
+  test "should not allow negative current_quantity" do
+    sl=StockLevel.new(valid.merge(current_quantity: -1))
+    assert_not sl.save
+  end
 
 
   private
