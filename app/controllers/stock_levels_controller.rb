e1e61115d957c0f095e658f25f31b08ff85471fd
Peter West <source@petewest.org>
2014-04-08 16:18:42 +0100

    Create new stock levels

--- app/controllers/stock_levels_controller.rb
+++ app/controllers/stock_levels_controller.rb
@@ -6,8 +6,26 @@ class StockLevelsController < ApplicationController
     @stock_levels=@product.stock_levels.all
   end
 
+  def new
+    @stock_level=@product.stock_levels.new
+  end
+
+  def create
+    @stock_level=@product.stock_levels.new(stock_level_params)
+    if @stock_level.save
+      flash[:success]="Stock added"
+      redirect_to product_stock_levels_url(@product)
+    else
+      flash.now[:danger]="Error saving stock"
+      render 'new'
+    end
+  end
+
   private
     def product_from_params
       @product=Product.find(params[:product_id])
+    end
+    def stock_level_params
+      params.require(:stock_level).permit(:due_at, :start_quantity, :current_quantity)
     end
 end
--- test/controllers/stock_levels_controller_test.rb
+++ test/controllers/stock_levels_controller_test.rb
@@ -22,6 +22,34 @@ class StockLevelsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get new" do
+    sign_in users(:seller)
+    get :new, product_id: @product.id
+    assert_response :success
+    assert_not_nil assigns(:stock_level)
+    assert_select "form"
+  end
+
+  test "should not get new as buyer" do
+    sign_in users(:buyer)
+    get :new, product_id: @product.id
+    assert_response :redirect
+  end
+
+  test "should create new stock when valid" do
+    sign_in users(:seller)
+    assert_difference "StockLevel.count" do
+      assert_difference "@product.stock_levels.count" do
+        post :create, product_id: @product.id, stock_level: valid
+      end
+    end
+    assert_redirected_to product_stock_levels_url(@product)
+  end
 
 
+
+  private
+    def valid
+      @stock_level||={due_at: 5.days.ago, start_quantity: 20}
+    end
 end
