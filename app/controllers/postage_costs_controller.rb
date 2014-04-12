8747ae91aedaa7721b42eaceefc113be14651674
Peter West <source@petewest.org>
2014-04-12 16:53:29 +0100

    New action

--- app/controllers/postage_costs_controller.rb
+++ app/controllers/postage_costs_controller.rb
@@ -1,6 +1,6 @@
 class PostageCostsController < ApplicationController
   before_action :signed_in_seller
-  before_action :postage_cost_from_params, only: [:edit]
+  before_action :postage_cost_from_params, only: [:edit, :update]
 
   def index
     @postage_costs=PostageCost.all
@@ -9,8 +9,26 @@ class PostageCostsController < ApplicationController
   def edit
   end
 
+  def update
+    if @postage_cost.update_attributes(postage_params)
+      flash[:success]="Postage cost updated"
+      redirect_to postage_costs_url
+    else
+      flash[:danger]="Postage cost update failed"
+      render 'edit'
+    end
+  end
+
+  def new
+    @postage_cost=PostageCost.new
+  end
+
   private
     def postage_cost_from_params
       @postage_cost=PostageCost.find(params[:id])
+    end
+
+    def postage_params
+      params.require(:postage_cost).permit(:from_weight, :to_weight, :unit_cost, :currency_id)
     end
 end
--- test/controllers/postage_costs_controller_test.rb
+++ test/controllers/postage_costs_controller_test.rb
@@ -19,6 +19,7 @@ class PostageCostsControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:postage_costs)
     assert_select "table"
+    assert_select "a[href=#{new_postage_cost_path}]"
   end
 
   test "should have an edit action" do
@@ -29,4 +30,36 @@ class PostageCostsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:postage_cost)
     assert_select "form"
   end
+
+  test "should update postage cost" do
+    sign_in users(:seller)
+    postage_cost=PostageCost.first
+    assert_not_equal postage_cost.from_weight, valid[:from_weight]
+    assert_not_equal postage_cost.to_weight, valid[:to_weight]
+    assert_not_equal postage_cost.unit_cost, valid[:unit_cost]
+    assert_not_equal postage_cost.currency_id, valid[:currency_id]
+    patch :update, id: postage_cost.id, postage_cost: valid
+    assert_not_nil assigns(:postage_cost)
+    assert_redirected_to postage_costs_url
+    postage_cost.reload
+    assert_equal valid[:from_weight], postage_cost.from_weight
+    assert_equal valid[:to_weight], postage_cost.to_weight
+    assert_equal valid[:unit_cost], postage_cost.unit_cost
+    assert_equal valid[:currency_id], postage_cost.currency_id
+  end
+
+  test "should have new action" do
+    sign_in users(:seller)
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:postage_cost)
+    assert assigns(:postage_cost).new_record?
+    assert_select "form"
+  end
+
+
+  private
+    def valid
+      @postage_cost||={from_weight: 50, to_weight: 80, currency_id: currencies(:usd).id, unit_cost: 80}
+    end
 end
