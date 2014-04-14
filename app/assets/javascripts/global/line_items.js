7421ddfddb998b3b025d68a5e708655a24e3508a
Peter West <source@petewest.org>
2014-04-14 15:04:31 +0100

    Switched to collection select, still not working completely, investigate

--- app/assets/javascripts/global/line_items.js
+++ app/assets/javascripts/global/line_items.js
@@ -1,5 +1,5 @@
 jQuery(function() {
-	jQuery('body').on('change', "input[data-submit-on-change=true]", function(e) {
+	jQuery('body').on('change', "input[data-submit-on-change=true],select[data-submit-on-change=true]", function(e) {
 		var $this=jQuery(this);
 		$this.parents('form').submit();
 	});
