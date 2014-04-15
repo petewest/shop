d8a6777c3e76b4aa7a89d31a200d55173ba8331d
Peter West <source@petewest.org>
2014-04-15 13:04:01 +0100

    Mailer preview

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
-
+  def confirmation
+    OrderMailer.confirmation_email(User.first, Order.first)
+  end
 end
