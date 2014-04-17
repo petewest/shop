ff0c744790e9897b5a2357650e06f4aed0aaa63b
Peter West <source@petewest.org>
2014-04-17 21:58:22 +0100

    Test for sub product cloning parent attributes

--- test/controllers/sub_products_controller_test.rb
+++ test/controllers/sub_products_controller_test.rb
@@ -9,6 +9,8 @@ class SubProductsControllerTest < ActionController::TestCase
     sign_in users(:seller)
     get :new, product_id: @product.id
     assert_response :success
+    assert_equal @product, assigns(:product)
+    assert_not_nil assigns(:sub_product)
   end
 
   test "should not get new page" do
@@ -26,6 +28,15 @@ class SubProductsControllerTest < ActionController::TestCase
     sign_in users(:seller)
     get :index, product_id: @product.id
     assert_response :success
+  end
+
+  test "should copy attributes (except name) from product to sub product" do
+    sign_in users(:seller)
+    get :new, product_id: @product.id
+    assert_not_equal @product.name, assigns(:sub_product).name
+    @product.attributes.symbolize_keys.except(:id, :updated_at, :created_at, :type, :name, :master_product_id).each do |key, value|
+      assert_equal value, assigns(:sub_product)[key], "#{key} mismatch"
+    end
   end
 
 end
