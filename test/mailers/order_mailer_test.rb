cf0a8096d8bc2defd38a714697b88e65367a4e6d
Peter West <source@petewest.org>
2014-04-17 22:33:10 +0100

    Flag to bcc seller on order creation (no controller to set it yet)

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create order confirmation" do
+    buyer=users(:buyer)
+    seller=users(:seller)
+    order=buyer.orders.first
+    email=OrderMailer.confirmation_email(buyer, order).deliver
+    assert_not ActionMailer::Base.deliveries.empty?
+    assert_equal [buyer.email], email.to
+    assert_equal [seller.email], email.from
+    assert email.bcc.include? seller.email
+    assert_not email.bcc.include? users(:seller2).email
+    assert_not email.bcc.include? users(:buyer_with_bcc).email
+  end
 end
