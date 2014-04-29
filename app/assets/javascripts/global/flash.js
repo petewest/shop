533e33fb65f69d14e33e52057f81ce0cd3279800
Peter West <source@petewest.org>
2014-04-29 19:53:40 +0100

    Can't use optional params in js that way :)

--- app/assets/javascripts/global/flash.js
+++ app/assets/javascripts/global/flash.js
@@ -2,6 +2,7 @@ function dismiss_flash() {
 	jQuery(".alert-dismissable").slideToggle(function() { jQuery(this).remove(); });
 }
 
-function dismiss_flash_timeout(time=5000) {
-	setTimeout(dismiss_flash, time);
+function dismiss_flash_timeout(time) {
+	timer=time || 5000;
+	setTimeout(dismiss_flash, timer);
 }
