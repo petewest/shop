7a8940d823a8b2fc7c286861bfc74e346a247b4c
Peter West <source@petewest.org>
2014-05-04 10:21:58 +0100

    Add caption to product images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501211008) do
+ActiveRecord::Schema.define(version: 20140504090357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140501211008) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "caption"
   end
 
   add_index "images", ["product_id"], name: "index_images_on_product_id", using: :btree
