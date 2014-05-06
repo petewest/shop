8f45a0f752e9c005b8217d1e5d4fbc31c3594d0a
Peter West <source@petewest.org>
2014-05-06 10:51:04 +0100

    Gift card product model (for creating available gift card denominations)

--- app/models/gift_card_product.rb
+++ app/models/gift_card_product.rb
@@ -1,2 +1,5 @@
 class GiftCardProduct < Product
+  def self.model_name
+    Product.model_name
+  end
 end
--- test/models/gift_card_product_test.rb
+++ test/models/gift_card_product_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class GiftCardProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should tell us it's a Product when asked" do
+    assert_equal Product.model_name, GiftCardProduct.model_name
+  end
 end
