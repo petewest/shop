a7a11f3bf349762be98d80d9f587ffe0d84c102c
Peter West <source@petewest.org>
2014-05-09 18:38:31 +0100

    Gift card product name change

--- app/models/gift_card_product.rb
+++ app/models/gift_card_product.rb
@@ -3,7 +3,8 @@ class GiftCardProduct < Product
   has_many :allocations, class_name: "GiftCardAllocation", foreign_key: "product_id"
 
   def name
-    self.class.model_name.human
+    with_cost=" #{ActiveSupport::NumberHelper::NumberToCurrencyConverter.convert(unit_cost/(10.0**currency.decimal_places), unit: currency.symbol)}" if unit_cost and currency
+    self.class.model_name.human + "#{with_cost}"
   end
 
 end
