51e325653d96c491364aa9b0bd322c626c63309c
Peter West <source@petewest.org>
2014-05-09 17:57:38 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140509144504) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "addresses", force: true do |t|
     t.string   "label"
     t.boolean  "default_billing"
