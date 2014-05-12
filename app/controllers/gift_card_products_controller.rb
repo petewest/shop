02c41ceef35f7ae82ed2f0e251df33bb8196c787
Peter West <source@petewest.org>
2014-05-12 21:27:23 +0100

    Don't show gift cards not for sale

--- app/controllers/gift_card_products_controller.rb
+++ app/controllers/gift_card_products_controller.rb
@@ -1,6 +1,6 @@
 class GiftCardProductsController < ApplicationController
   before_action :signed_in_user
   def index
-    @gift_card_products=GiftCardProduct.all
+    @gift_card_products=GiftCardProduct.for_sale.all
   end
 end
--- test/controllers/gift_card_products_controller_test.rb
+++ test/controllers/gift_card_products_controller_test.rb
@@ -9,6 +9,13 @@ class GiftCardProductsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:gift_card_products)
   end
 
+  test "should not show gift cards no longer for sale" do
+    sign_in users(:buyer)
+    get :index
+    assert_response :success
+    assert_not assigns(:gift_card_products).include?(products(:gift_card_not_for_sale))
+  end
+
   test "should not have index action for anonymous" do
     get :index
     assert_redirected_to signin_path
